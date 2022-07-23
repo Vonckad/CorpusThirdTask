08119f2df8ebd4d62701bfe3c864a6d1fa288fab
Vonckad <Vonkad@yandex.ru>
2022-07-23 21:41:02 +0300

    add modePickerView

--- CorpusThirdTask/ViewControllers/ViewController.swift
+++ CorpusThirdTask/ViewControllers/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         var titleLabel: UILabel = UILabel(frame: .zero)
         view.addSubview(titleLabel)
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.font = .systemFont(ofSize: 20)
         NSLayoutConstraint.activate([
             titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
             titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
@@ -26,7 +27,7 @@ class ViewController: UIViewController {
         view.addSubview(textView)
         textView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            textView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 24),
+            textView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
             textView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 24),
             textView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -24),
             textView.heightAnchor.constraint(equalToConstant: view.frame.height / 4)
@@ -36,7 +37,7 @@ class ViewController: UIViewController {
         textView.backgroundColor = .white
         textView.keyboardDismissMode = .onDrag
         textView.font = .systemFont(ofSize: 20)
-        textView.layer.cornerRadius = 20
+        textView.layer.cornerRadius = 5
         return textView
     }()
     
@@ -45,11 +46,28 @@ class ViewController: UIViewController {
         view.addSubview(segmentedControl)
         segmentedControl.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            segmentedControl.topAnchor.constraint(equalTo: textView.bottomAnchor, constant: 24),
+            segmentedControl.topAnchor.constraint(equalTo: textView.bottomAnchor, constant: 8),
             segmentedControl.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 24),
             segmentedControl.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -24)
         ])
         return segmentedControl
+    }()
+    
+    lazy var modePickerView: UIPickerView = {
+        var modePickerView = UIPickerView()
+        view.addSubview(modePickerView)
+        modePickerView.translatesAutoresizingMaskIntoConstraints = false
+        modePickerView.delegate = self
+        modePickerView.dataSource = self
+        modePickerView.layer.cornerRadius = 5
+        modePickerView.setContentHuggingPriority(.defaultLow, for: .vertical)
+        NSLayoutConstraint.activate([
+            modePickerView.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 4),
+            modePickerView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 24),
+            modePickerView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -24),
+            modePickerView.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        return modePickerView
     }()
     
     lazy var sendButton: UIButton = {
@@ -57,7 +75,7 @@ class ViewController: UIViewController {
         view.addSubview(sendButton)
         sendButton.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            sendButton.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 24),
+            sendButton.topAnchor.constraint(equalTo: modePickerView.bottomAnchor, constant: 24),
             sendButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 24),
             sendButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -24),
         ])
@@ -69,6 +87,7 @@ class ViewController: UIViewController {
         return sendButton
     }()
     
+    var modeArray = ["radiobutton1", "radiobutton2", "radiobutton3"]
     var lastText = ""
     
     var result: ResponceModel? {
@@ -85,15 +104,17 @@ class ViewController: UIViewController {
         titleLabel.text = "Калі ласка, увядзіце тэкст"
         textView.text = ""
         segmentedControl.selectedSegmentIndex = 0
+        modePickerView.backgroundColor = .white
         sendButton.addTarget(self, action: #selector(send), for: .touchUpInside)
         view.backgroundColor = .lightGray
     }
     @objc func send() {
         guard let selector = segmentedControl.titleForSegment(at: segmentedControl.selectedSegmentIndex) else { return }
         guard let text = textView.text else { return }
+        let mode = modeArray[modePickerView.selectedRow(inComponent: 0)]
         if !text.isEmpty {
             lastText = text
-            postText(text: text, checkbox1: 0, checkbox2: 0, checkbox3: 1, mode: "radiobutton2", selector: selector)
+            postText(text: text, checkbox1: 0, checkbox2: 0, checkbox3: 1, mode: mode, selector: selector)
         textView.resignFirstResponder()
         } else {
         
@@ -121,5 +142,26 @@ extension ViewController: UITextViewDelegate {
         sendButton.isEnabled = true
         textView.text = lastText
         textView.backgroundColor = .white
+    }
+}
+
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        modeArray.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let label = (view as? UILabel) ?? UILabel()
+        
+        label.textColor = .red
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.text = modeArray[row]
+        
+        return label
     }
 }
