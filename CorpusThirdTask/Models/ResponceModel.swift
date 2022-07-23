fea7e25637e9443df456e03ecc11eb3d5ed38b89
Vonckad <Vonkad@yandex.ru>
2022-07-23 19:58:06 +0300

    add StringExtension and create ResponceModel

---
+++ CorpusThirdTask/Models/ResponceModel.swift
@@ -0,0 +1,13 @@
+//
+//  ResponceModel.swift
+//  CorpusThirdTask
+//
+//  Created by Vlad Ralovich on 23.07.22.
+//
+
+import Foundation
+
+struct ResponceModel: Decodable {
+    var text: String
+    var result: String
+}
